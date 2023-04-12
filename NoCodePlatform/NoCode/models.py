72d31b6685e8e5bb2a326aa17c5550ccd8e179fb
Alexandr Perepelkin <pepelr73@gmail.com>
2023-04-13 01:47:11 +0300

    many things

--- NoCodePlatform/NoCode/models.py
+++ NoCodePlatform/NoCode/models.py
@@ -9,3 +9,10 @@ class Figure(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+class Size(models.Model):
+    name = models.CharField(max_length=60)
+    create_time = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
--- NoCodePlatform/NoCode/views.py
+++ NoCodePlatform/NoCode/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+from NoCode.models import *
 
 def index(request):
-    return render(request, 'NoCode/index.html')
+    figures = Figure.objects.all()
+    return render(request, 'NoCode/index.html', {'figures' : figures})
 
+# third = {"code":'''
+#             const geometry = new THREE.SphereGeometry( 2, 30, 30 );
+# 			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+# 			const fig = new THREE.Mesh( geometry, material );
+# 			scene.add( fig );'''}
+def ThirdDimension(request):
+    fig = request.GET.get("name")
+    third = {"code":Figure.objects.get(name=fig).code}
+    return render(request, 'NoCode/ThirdDimansionPage.html', third)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
